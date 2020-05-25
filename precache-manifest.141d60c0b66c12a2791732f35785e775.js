3cee91ea1ae03cfa09d704e7cd464b043d3fb508
Dmytro Malon <46137635+dieie32@users.noreply.github.com>
2020-05-26 01:34:43 +0300

    Updates

---
+++ precache-manifest.141d60c0b66c12a2791732f35785e775.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665d7459bde46419a191eff0eb02eef5",
+    "url": "/socialent/index.html"
+  },
+  {
+    "revision": "d9d57ab78fe81417e2d3",
+    "url": "/socialent/static/css/main.4f46e397.chunk.css"
+  },
+  {
+    "revision": "ff972faa60010c79b5db",
+    "url": "/socialent/static/js/2.1d4a51d7.chunk.js"
+  },
+  {
+    "revision": "8debe1713c30c1c8fa79b57451984727",
+    "url": "/socialent/static/js/2.1d4a51d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9d57ab78fe81417e2d3",
+    "url": "/socialent/static/js/main.df00c32b.chunk.js"
+  },
+  {
+    "revision": "b128092a76e263139940",
+    "url": "/socialent/static/js/runtime-main.176f1d56.js"
+  },
+  {
+    "revision": "07e6a6cd3cdb9eedaa9aa81fafe1e42a",
+    "url": "/socialent/static/media/Gilroy-ExtraBold.07e6a6cd.ttf"
+  },
+  {
+    "revision": "2037b7c09569dd6f0ca302d92ec0c23e",
+    "url": "/socialent/static/media/Gilroy-ExtraBold.2037b7c0.woff"
+  },
+  {
+    "revision": "bdf3ea41ca5484b3f973d14a46cad4a4",
+    "url": "/socialent/static/media/Gilroy-ExtraBold.bdf3ea41.eot"
+  },
+  {
+    "revision": "59525ce19c9e4999bc19d96e4a88c1b3",
+    "url": "/socialent/static/media/Gilroy-Regular.59525ce1.eot"
+  },
+  {
+    "revision": "8e8705d069fbb9099b3a1e1d779fa5c5",
+    "url": "/socialent/static/media/Gilroy-Regular.8e8705d0.woff"
+  },
+  {
+    "revision": "ae5e7255973ffe09b53f07a2805232a8",
+    "url": "/socialent/static/media/Gilroy-Regular.ae5e7255.ttf"
+  },
+  {
+    "revision": "cba5b64fe31318ca116ab716d9fa6510",
+    "url": "/socialent/static/media/Gopher-Bold.cba5b64f.ttf"
+  },
+  {
+    "revision": "fb9908aa3d1155ee6e10891b9dfb4a65",
+    "url": "/socialent/static/media/Gopher-Bold.fb9908aa.otf"
+  },
+  {
+    "revision": "154e44879af4bf0592beebb4c7b09252",
+    "url": "/socialent/static/media/Gopher-Regular.154e4487.ttf"
+  },
+  {
+    "revision": "bb12a0a1616902b494e6852ce24c7e2a",
+    "url": "/socialent/static/media/Gopher-Regular.bb12a0a1.otf"
+  },
+  {
+    "revision": "278b159c92cc1d6ac256d312bcb9bd97",
+    "url": "/socialent/static/media/HelveticaBold.278b159c.eot"
+  },
+  {
+    "revision": "7a242cbef983a4973faa81ebf5e5be18",
+    "url": "/socialent/static/media/HelveticaBold.7a242cbe.ttf"
+  },
+  {
+    "revision": "c8af73bb79b69b1e860d7d9425baac4e",
+    "url": "/socialent/static/media/HelveticaBold.c8af73bb.woff"
+  },
+  {
+    "revision": "16d1134d083c802ccac76e97b5a9c0c7",
+    "url": "/socialent/static/media/Instagram-1.16d1134d.png"
+  },
+  {
+    "revision": "94b8f26792ab4eee66cf7da75070d4c9",
+    "url": "/socialent/static/media/Instagram-2.94b8f267.png"
+  },
+  {
+    "revision": "9aabf4b5b9df93fefc1c8bbad84038fa",
+    "url": "/socialent/static/media/chill-man.9aabf4b5.png"
+  },
+  {
+    "revision": "0a482ee9d614a89ea9611435699c8dc7",
+    "url": "/socialent/static/media/langing-page-main.0a482ee9.png"
+  },
+  {
+    "revision": "85b02561ad95b1894df00485cb906811",
+    "url": "/socialent/static/media/login-man.85b02561.png"
+  },
+  {
+    "revision": "d309be1818ca1521f7d5a7e84111031a",
+    "url": "/socialent/static/media/main-page-man.d309be18.png"
+  }
+]);
